0877d20bcabd1b26581b72e4cb15a778116434a3
MarufSwe <khanmaruf.swe@gmail.com>
2023-01-24 01:34:22 +0600

    post crud added

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import \
-    (ListView,
-     DetailView,
-     CreateView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+ListView,
+DetailView,
+CreateView,
+UpdateView,
+DeleteView
+)
 from .models import Post
 
 # from django.http import HttpResponse
@@ -28,16 +32,47 @@ class PostDetailView(DetailView):
     template_name = 'blog_app/post_details.html'
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
-    template_name = 'blog_app/post_form.html'
+    # template_name = 'blog_app/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
+# LoginRequiredMixin = is user logged in?
+# UserPassesTestMixin = user can update only own post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    # template_name = 'blog_app/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # can edit current logged in user
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/' #after delete going to home page
+    # template_name = 'blog_app/post_confirm_delete.html'
+
+    # can delete current logged in user
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
